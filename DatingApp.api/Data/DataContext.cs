70cfd483a6aa5ec6e0eb5004f8ae9073e870095c
Anas Avais <Anasavais@hotmail.com>
2019-09-16 22:58:34 +0500

    initial Commit

---
+++ DatingApp.api/Data/DataContext.cs
@@ -0,0 +1,19 @@
+using DatingApp.api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.api.Data
+{
+    // public class DataContext :DbContext
+    // {
+    //     public DataContext(DbContextOptions<DataContext>options):base(options)
+    //     {
+            
+    //     }
+    // }
+    public class DataContext : DbContext
+    {
+     public DataContext(DbContextOptions<DataContext> options) :base(options) {}
+
+     public DbSet<Value>  Values { get; set; }   
+    }
+}
