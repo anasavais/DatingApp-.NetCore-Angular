8b49fe9acdb351e86acea4922961c704d7c2c8ad
Anas Avais <Anasavais@hotmail.com>
2019-10-22 20:36:52 +0500

    Chapter 4

---
+++ DatingApp-Spa/src/app/register/register.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/Auth.service';
+import { error } from '@angular/compiler/src/util';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Input() valuesFromHome: any;
+  @Output() CancelRegister = new EventEmitter();
+model: any = {};
+  constructor(private auth: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  Register() {
+    this.auth.register(this.model).subscribe(() => {
+      console.log('Successful Registration');
+    },
+     Error => {
+       console.log(error);
+      }
+      );
+    console.log(this.model);
+  }
+
+  Cancel() {
+   this.CancelRegister.emit(false);
+    console.log(this.model);
+  }
+
+
+
+}
